4fbb5057380920fc1a26cc3aa1b86933809b8835
Dwarnin <50577223+Dwarnin@users.noreply.github.com>
2019-05-14 15:17:33 +0300

    add slider

--- app/js/common.js
+++ app/js/common.js
@@ -10,11 +10,18 @@ $(document).ready(function() {
 
 $('.slider_container').slick({
 	infinite: true,
-	slidesToShow: 3,
+	slidesToShow: 1,
 	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 1000,
-	speed: 200,
-	dots: true,
+	// autoplay: true,
+	// autoplaySpeed: 1000,
+	// speed: 200,
 	swipeToSlide: true
+	// asNavFor: '.slider_nav'
 });
+// $('.slider_nav').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: '.slider-for',
+//   centerMode: true,
+//   focusOnSelect: true
+// });
